61c336901ee65ef3cae3c92fe4fb05f9c5a09007
Bibekkun <bibekrai655@gmail.com>
2023-03-19 22:27:48 +0545

    emotion detection

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
 
-# Load the pre-trained face detection classifier (Haar cascade)
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# Load the image to be detected
-img = cv2.imread('img1.jpeg')
+emotion_model = load_model('fer2013_mini_XCEPTION.102-0.66.hdf5', compile=False)
 
-# Convert the image to grayscale
+EMOTIONS = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise"]
+
+img = cv2.imread('img5.jpg')
+
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Detect faces in the grayscale image
 faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
-# Draw rectangles around the detected faces
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
     total_pixels = w * h
@@ -20,7 +21,16 @@ for (x, y, w, h) in faces:
     face_percentage = face_pixels / total_pixels * 100
     cv2.putText(img, f"{face_percentage:.2f}%", (x, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
 
-# Display the resulting image
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_gray = cv2.resize(roi_gray, (64, 64))
+    roi_gray = np.expand_dims(roi_gray, axis=-1)
+    roi_gray = np.expand_dims(roi_gray, axis=0)
+    roi_gray = roi_gray / 255.0
+
+    preds = emotion_model.predict(roi_gray)[0]
+    emotion_label = EMOTIONS[np.argmax(preds)]
+    emotion_confidence = preds[np.argmax(preds)]
+
 cv2.imshow('Face Detection', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
